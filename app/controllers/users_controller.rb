80b1b8ebd3142561bf139d10981081c43f02d01c
Kevin Kelley <kkelley622@gmail.com>
2023-02-26 16:55:16 -0600

    Upddated users routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
-    before_action :find_user, only: [:show, :update, :destroy]
-     
+    before_action :find_user, only: [:show, :destroy]
+
     def index 
         users = User.all
         render json: users, status: :ok 
@@ -14,16 +14,6 @@ class UsersController < ApplicationController
     def create 
         user = User.create!(user_params)
         render json: user, status: :created
-    end
-
-    def update 
-        @user.update!(user_params)
-        render json: @user, status: 202
-    end
-
-    def destroy
-        @user.destroy 
-        head :no_content 
     end
 
     private 
