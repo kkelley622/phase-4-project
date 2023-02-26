3b917b720e32de1e7d0c3365ace2b44322f8431c
Kevin Kelley <kkelley622@gmail.com>
2023-02-26 16:47:02 -0600

    Added RESTful route methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+    before_action :find_user, only: [:show, :update, :destroy]
+     
+    def index 
+        users = User.all
+        render json: users, status: :ok 
+    end
+
+    def show 
+        render json: @user, status: :ok
+    end
+
+    def create 
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    def update 
+        @user.update!(user_params)
+        render json: @user, status: 202
+    end
+
+    def destroy
+        @user.destroy 
+        head :no_content 
+    end
+
+    private 
+
+    def find_user 
+        @user = User.find(params[:id])
+    end 
+
+    def user_params 
+        params.permit(:username, :first_name, :last_name, :password)
+    end
 end
