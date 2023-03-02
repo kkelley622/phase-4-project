c95899be1f4de2c6713073973e80fb2b70a3b992
Kevin Kelley <kkelley622@gmail.com>
2023-03-02 17:38:06 -0600

    Login form outlined, not functioning

--- client/src/users/Login.jsx
+++ client/src/users/Login.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <form>
+      <label>Username</label>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        required
+      />
+      <label>Password</label>
+      <input
+        type="text"
+        name="password"
+        value={formData.password}
+        required
+      />
+      <input type="submit" />
+    </form>
   )
 }
 
