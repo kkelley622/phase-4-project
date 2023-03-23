9ddf1521a5e0af91d8acf8a82d3bba20085dabfc
Kevin Kelley <kkelley622@gmail.com>
2023-03-23 16:40:42 -0500

    removed not needed fetch in UsersReviews, changed to filtering through all reviews

--- client/src/reviews/UserReviews.jsx
+++ client/src/reviews/UserReviews.jsx
@@ -1,32 +1,34 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import { UsersContext } from '../context/UsersContext';
 import { ReviewsContext } from '../context/ReviewsContext';
 import ReviewCard from './ReviewCard';
+import { ErrorsContext } from '../context/ErrorsContext';
 
 const UserReviews = () => {
 
     const navigate = useNavigate();
     const {user_id} = useParams();
-    const [userReviews, setUserReviews] = useState([]);
     const {loggedIn, loading} = useContext(UsersContext);
-    const {handleDeleteReview} = useContext(ReviewsContext);
-
-
-
+    const {handleDeleteReview, reviews} = useContext(ReviewsContext);
+    const {setErrors} = useContext(ErrorsContext);
+    
     useEffect(() => {
         if(!loading && !loggedIn){
            navigate("/login")
            }
-            fetch("/users/" + user_id + "/reviews")
-            .then(response => response.json())
-            .then(data => setUserReviews(data))
-    }, [user_id, loggedIn, loading, navigate, userReviews]);
+            setErrors([]);
+    }, [ loggedIn, loading, navigate]);
 
-    const reviewCards = userReviews?.map(review => <ReviewCard key={review.id} review={review} handleDeleteReview={handleDeleteReview}/>);
+    const currentReviews = reviews.filter((review) => review.user_id == user_id)
+
+    const currentReviewCards = currentReviews?.map(review => <ReviewCard key={review.id} review={review} handleDeleteReview={handleDeleteReview}/>);
 
   return (
-    <div>{ reviewCards }</div>
+    <div>
+
+      { currentReviewCards }
+      </div>
   )
 }
 
